db3a60ecf2958b42a51431d33fb2dc463fc346ee
emrecancc <emrecancerli55@gmail.com>
2026-07-02 22:50:22 +0300

    chore: devmend retry hotfix #86

--- src/__tests__/snapshot.test.tsx
+++ src/__tests__/snapshot.test.tsx
@@ -1,7 +1,10 @@
+import React from 'react';
 import { render } from '@testing-library/react';
-import { Dashboard } from '../Dashboard';
+import Dashboard from '../Dashboard';
 
 test('Dashboard renders correctly', () => {
-  const { asFragment } = render(<Dashboard />);
-  expect(asFragment()).toMatchSnapshot(); // fails when date changes
+  const dateNowSpy = jest.spyOn(Date, 'now').mockReturnValue(new Date('2024-01-14T00:00:00Z').getTime());
+  const { container } = render(<Dashboard />);
+  expect(container).toMatchSnapshot();
+  dateNowSpy.mockRestore();
 });
