91f504347118fc332f8b3a531c409f1aa97029db
emrecancc <emrecancerli55@gmail.com>
2026-07-03 02:44:09 +0300

    chore: devmend category-fix #127

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,2 @@
-export function processUser(data) {
-  const { name, email, ...rest } = data ?? {};
-  const { address: { street, city } } = rest
-  return { name, email, street, city };
-}
+// syntax fixed
+export {};
