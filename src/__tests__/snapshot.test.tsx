7086420202adcbae7ea1d9ef7f726f6c8b2384fa
emrecancc <emrecancerli55@gmail.com>
2026-07-02 22:53:28 +0300

    ci: introduce flaky_snapshot (s156)

--- src/__tests__/snapshot.test.tsx
+++ src/__tests__/snapshot.test.tsx
@@ -1,10 +1,7 @@
-import React from 'react';
 import { render } from '@testing-library/react';
-import Dashboard from '../Dashboard';
+import { Dashboard } from '../Dashboard';
 
 test('Dashboard renders correctly', () => {
-  const dateNowSpy = jest.spyOn(Date, 'now').mockReturnValue(new Date('2024-01-14T00:00:00Z').getTime());
-  const { container } = render(<Dashboard />);
-  expect(container).toMatchSnapshot();
-  dateNowSpy.mockRestore();
+  const { asFragment } = render(<Dashboard />);
+  expect(asFragment()).toMatchSnapshot(); // fails when date changes
 });
